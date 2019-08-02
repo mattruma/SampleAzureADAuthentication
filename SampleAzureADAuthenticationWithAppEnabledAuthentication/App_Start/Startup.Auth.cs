33555a0628305f43c8965d3f2dc0769e23b3b9db
Matt Ruma <mattruma@gmail.com>
2019-08-02 16:12:31 -0400

    added portal managed auth example

---
+++ SampleAzureADAuthenticationWithAppEnabledAuthentication/App_Start/Startup.Auth.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+
+namespace SampleAzureADAuthenticationWithAppEnabledAuthentication
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            return;
+        }
+    }
+}
---
+++ SampleAzureADAuthenticationWithAppEnabledAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SampleAzureADAuthenticationWithAppEnabledAuthentication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View((ClaimsPrincipal)this.User);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
